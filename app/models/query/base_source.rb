6ee5ce160744613447553665f6bcdc6be8f018b3
Vanessa Li <vanessa.li@rd.io>
2013-11-15 18:48:25 -0800

    actually save changes

--- app/models/query/base_source.rb
+++ app/models/query/base_source.rb
@@ -16,8 +16,12 @@ module Query
     # xpath => a list of xpath objects 
     # e.g. {:exp => "//items/item", :name => :content, :select => lambda { |n| n.text }}
     #
-    def make_request(params, xpath_objects, count=1)
-      result = []
+    def make_request(params, xpath_objects)
+      result = {}
+      # if params.length > 0
+      #   params = URI::encode(params)
+      #   api_query = "#{self.api_url}?#{params}"
+      # else
       api_query = "#{self.api_url}?#{params}"
       params = URI::encode(api_query)
       document = Nokogiri::XML(open(api_query))
@@ -26,9 +30,7 @@ module Query
       xpath_objects.each do |item|
         selected = document.xpath( item[:exp] )
         if selected.size > 0
-          for i in 0..count
-            result << {item[:name] => item[:select].call(selected[i])}
-          end
+          result[item[:name]] = item[:select].call(selected[0])
         end
       end
       return result
